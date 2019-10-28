c7d06905f83f2ade9ffdbac2bbff2170fd2a3671
jogkunal5 <jogkunal5@gmail.com>
2019-10-28 19:54:41 +0530

    Implemented routing using withRouter for menu-items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (<div><h1>HATS PAGE</h1></div>)
 
 function App() {
+
+  // exact: it means that path must be exactly same in order to render component
   return (
-    <div className>
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
